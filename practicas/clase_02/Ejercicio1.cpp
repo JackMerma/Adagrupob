5279c8d7f728bfca72b8e1e3624ff382ed686bb6
Jackson Merma <jmermap@unsa.edu.pe>
2021-09-20 22:39:06 -0500

    ejercicio 1 resuelto

---
+++ practicas/clase_02/Ejercicio1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int n,k,count=0;
+
+  cin>>n;cin>>k;
+
+  for(int i=0;i<n;i++){
+    int num;
+    cin>>num;
+    count+=(num!=0&&num%k==0)?1:0;
+  }
+  cout<<count;
+
+  return 0;
+}
