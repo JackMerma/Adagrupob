873e538e8a82f0a8bd6f64c80b313541052d71b3
Jackson Merma <jmermap@unsa.edu.pe>
2021-10-20 21:15:37 -0500

    Correcion del algoritmo, entrada de datos y comentarios modificados

--- practicas/clase_04/sumar_par.cpp
+++ practicas/clase_04/sumar_par.cpp
@@ -2,7 +2,7 @@
 * @author      : Jackson Fernando Merma Portocarrero (jmermap@unsa.edu.pe)
 * @created     : 18/10/2021
 * @filename    : sumar_par
-* @description : Imprime la suma del dato i-ésimo par del arreglo con todas las posiciones u-ésimas del mismo arreglo
+* @description : Imprime la suma del dato i-ésimo par del arreglo con todas las posiciones del mismo arreglo
 */
 #include <bits/stdc++.h>
 using namespace std;
@@ -13,16 +13,20 @@ int main(){
 	cin.tie(0);
 
 	//codigo
-	//caso de prueba
-	int arr[]={7,6,5};//arreglo
-	int len=3;//longitud del arreglo
+	//entrada de datos
+	int n;
+	cin>>n;
+	int arr[n]={0};
+
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
 
 	//algoritmo
-	for(int i=0;i<len;i++){//recorre n elementos (0...n-1)
-		if(i%2==0){//si el elemento es par 
-			for(int u=0;u<len;u++){//recorre n elemento (0...n-1)s
+	for(int i=0;i<n;i++){//recorre n elementos (0...n-1)
+		if(arr[i]%2==0){//si el elemento es par 
+			for(int u=0;u<n;u++){//recorre n elemento (0...n-1)s
 				//imprime la suma del par mas la pos u-ésima del arreglo
-				cout<<(arr[u]+arr[i])<<"\n"; 
+				cout<<arr[i]<<"+"<<arr[u]<<"="<<(arr[u]+arr[i])<<"\n"; 
 			}
 		}
 	}
