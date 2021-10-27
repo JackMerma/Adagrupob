7fd0e979384eb681f6b3e939e07d1abbb80ffd30
Jackson Merma <jmermap@unsa.edu.pe>
2021-10-27 17:09:22 -0500

    Question 1 - O(n), algoritmo y explicacion

---
+++ practicas/clase_05/12_ejercicios/question01_.cpp
@@ -0,0 +1,44 @@
+/** 
+* @author      : Jackson Fernando Merma Portocarrero (jmermap@unsa.edu.pe)
+* @created     : 27/10/2021
+* @filename    : question01_
+* @description : Respondiendo a:
+*
+*      Review of Time Complexity
+*      Q1: What is the time complexity of
+*      for (i = 0; i < n; i++) {
+*        statement;
+*      }
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;//casos
+	cin>>n;
+
+
+
+	//algoritmo
+	for(int i=0;i<n;i++){
+		//statement
+		cout<<"hola!"<<endl;
+	}
+
+	/*
+	 * Complejidad del algoritmo --> lineal --> O(n)
+	 * 
+	 * Explicacion: Recorre un bucle 'n' veces
+	 */
+
+	
+
+	return 0;
+}
+
