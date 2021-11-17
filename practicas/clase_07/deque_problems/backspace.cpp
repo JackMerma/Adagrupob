b937fee74d47dbb8b04749da4b31c0595a1de67f
Jackson Merma <jmermap@unsa.edu.pe>
2021-11-17 12:25:38 -0500

    documentacion de backspace.cpp y comentarios de proceso (casos)

--- practicas/clase_07/deque_problems/backspace.cpp
+++ practicas/clase_07/deque_problems/backspace.cpp
@@ -2,7 +2,7 @@
 * @author      : Jackson Fernando Merma Portocarrero (jmermap@unsa.edu.pe)
 * @created     : 17/11/2021
 * @filename    : backspace
-* @description : _
+* @description : dada una cadena, elimina el ultimo caracter de haber un #
 */
 #include <bits/stdc++.h>
 using namespace std;
@@ -12,36 +12,45 @@ using namespace std;
 #define endl "\n"
 #define PI 3.1415926535897932384626
 
-void init_code(){
-#ifndef ONLINE_JUDGE
-	freopen("input.txt","r",stdin);
-	freopen("output.txt","w",stdout);
-#endif
-}
+//prototipo
+void back_space(string);
 
 int main(){
+	/**
+	 *  Metodo principal
+	 */
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	init_code();
 	
-	string str;cin>>str;
-	deque<char> de;
-
-	for(int i=0;i<str.length();i++){
-		if(str[i]!='#'){
-			de.push_back(str[i]);
-		}else{
-			if(!de.empty()){
-				de.pop_back();
-			}
-		}
-	}
-
-	while(!de.empty()){
-		cout<<de.front();
-		de.pop_front();
-	}
+	//lamado a la funcion back_space
+	back_space("abc#de##f#ghi#jklmn#op#");
+	back_space("abcd###");
+	back_space("abcd#e#f#g#");
 
 	return 0;
 }
 
+void back_space(string expression){
+	/**
+	 *  Metodo back_space: realiza el proceso back_space con la expresion enviada
+	 *  Atributos:
+	 *  	expression -> cadena a realizar el back_space 
+	 */
+	deque<char> deque_letters;//deque principal
+
+	for(int i=0;i<expression.length();i++){//se recorre toda la cadena
+		if(expression[i]!='#'){//si NO #, se agrega al final
+			deque_letters.push_back(expression[i]);
+		}else{//si es #, se elimina siempre que haya algun dato en el deque
+			if(!deque_letters.empty()){
+				deque_letters.pop_back();
+			}
+		}
+	}
+
+	while(!deque_letters.empty()){//se recorre el resultado del deque
+		cout<<deque_letters.front();
+		deque_letters.pop_front();
+	}
+	cout<<endl;
+}
