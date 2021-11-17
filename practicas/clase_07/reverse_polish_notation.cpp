373042a821fd9bdae16bfc3c2a348c9d990750d2
Jackson Merma <jmermap@unsa.edu.pe>
2021-11-16 22:33:03 -0500

    documentacion de metodos y comentarios en proceso reverse_polish_notatio.cpp

--- practicas/clase_07/reverse_polish_notation.cpp
+++ practicas/clase_07/reverse_polish_notation.cpp
@@ -17,51 +17,70 @@ using namespace std;
 int operate(string, int, int);
 int getResult(string);
 
-void init_code(){
-#ifndef ONLINE_JUDGE
-	freopen("input.txt","r",stdin);
-	freopen("output.txt","w",stdout);
-#endif
-}
-
 int main(){
+	/**
+	 * Metodo principal
+	 */
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	init_code();
 	
-	int result1 = getResult("2 3 +");
-	cout << "Resultado 1: " << result1 << endl;
+	//casos
+
+	int result1 = getResult("2 1 + 3 *");
+	cout<<"Resultado 1:"<<result1<<endl;
+
+	int result2 = getResult("4 13 5 / +");
+	cout<<"Resultado 2:"<<result2<<endl;
+
+	int result3 = getResult("10 6 9 3 + -11 * / * 17 + 5 +");
+	cout<<"Resultado 3:"<<result3<<endl;
+
 
 	return 0;
 }
 
 int getResult(string expression){
-	expression += " ";
-	string data;
-	int space=expression.find(" ", 0);
-	stack<string> stack_expression;
+	/**
+	 * Metodo getResult: procesa la expresion (cadena)
+	 * Atributos:
+	 * 	expression -> cadena a procesar
+	 */
+	expression += " "; //para no obtener errores
 
-	while(space!=-1){
-		string data=expression.substr(0,space); 
+	string data;//numero o operacion
+	stack<string> stack_expression;//stack principal
 
-		if(data!="+"&&data!="-"&&data!="*"&&data!="/"){
+	int space=expression.find(" ", 0);//coincidencia con el primer espacio
+
+	while(space!=-1){//siempre que haya algo mas que agregar
+
+		string data=expression.substr(0,space);//se obtiene la data
+
+		if(data!="+"&&data!="-"&&data!="*"&&data!="/"){//si es numero, solo se agrega
 			stack_expression.push(data);
-		}else{
+		}else{//si es operador, se opera con los dos ultimos numeros del stack
 			int b,a;
 			b=stoi(stack_expression.top());stack_expression.pop();
 			a=stoi(stack_expression.top());stack_expression.pop();
 
-			stack_expression.push(to_string(operate(data,a,b)));
+			stack_expression.push(to_string(operate(data,a,b)));//se coloca el resultado en el stack
 		}
 
-		expression=expression.substr(space+1,expression.length()-space-1);
-		space=expression.find(" ", 0);
+		expression=expression.substr(space+1,expression.length()-space-1);//eliminacion de la primera subcadena
+		space=expression.find(" ", 0);//se vuelve a buscar un espacio
 	}
 
-	return stoi(stack_expression.top());
+	return stoi(stack_expression.top());//el resultado sera el unico dato en el stack
 }
 
 int operate(string sig, int a,int b){
+	/**
+	 *	Metodo operate: opera dos numeros, respecto al operador enviado
+	 *	Atributos:
+	 *		sig -> el operador enviado
+	 *		a   -> primero numero
+	 *		b   -> segundo numero
+	 */
 	if(sig=="+"){
 		return a+b;
 	}else if(sig=="-"){
@@ -72,4 +91,3 @@ int operate(string sig, int a,int b){
 		return a/b;
 	}
 }
-
