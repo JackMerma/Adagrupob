ff5d082b6498964374e914ffe952934346fcb313
Jackson Merma <jmermap@unsa.edu.pe>
2021-12-08 10:54:13 -0500

    implementacion del problema a vicious pikeman y resultado del juez online

---
+++ practicas/lab_9_greedyAlgorithms/AViciousPikeman/aViciousPikeman.cpp
@@ -0,0 +1,50 @@
+/** 
+* @author      : Jackson Fernando Merma Portocarrero (jmermap@unsa.edu.pe)
+* @filename    : bankQueue
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define li long int
+#define endl "\n"
+#define PI 3.1415926535897932384626
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, t;
+	cin>>n>>t;//entra de n y t
+
+	int A,B,C,t0;//entrada de A, B, C y t_0
+	cin>>A>>B>>C>>t0;
+
+	vector<li> time_taken;//tiempo que toma cada problema
+	time_taken.push_back(t0);//el primero
+
+	//llenando los tiempos tomados en los problemas
+	for(int i=1;i<n;i++){
+		//el tiempo desde el segundo problema, usa la formula
+		time_taken.push_back(((A*time_taken[i-1]+B)%C)+1);
+	}
+
+	//se ordena de menor a mayor (menores tiempos tomados)
+	sort(time_taken.begin(), time_taken.end());
+
+	int solved=0, all_time_taken_ans=0;
+	li all_time_taken=0;
+
+	//se recorre siempre que hayan preguntas
+	//y que no se sobrepase el tiempo dado
+	for(int i=0;i<n && all_time_taken+time_taken[i]<=t;i++){
+		solved++;//cantidad de preguntas resueltas
+		all_time_taken_ans=(all_time_taken_ans+all_time_taken+time_taken[i])%1000000007;//total de tiempo tomado con el MOD
+		all_time_taken+=time_taken[i];//total de tiempo tomado
+	}
+
+	cout<<solved<<" "<<all_time_taken_ans<<endl;
+
+	return 0;
+}
