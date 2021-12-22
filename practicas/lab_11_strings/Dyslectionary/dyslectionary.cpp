6d3c4a7f547c8ec36aa7813d54b4a204f478ddd2
Jackson Merma <jmermap@unsa.edu.pe>
2021-12-22 15:34:13 -0500

    documentacion y comentarios agregados en dyslectionary.cpp

--- practicas/lab_11_strings/Dyslectionary/dyslectionary.cpp
+++ practicas/lab_11_strings/Dyslectionary/dyslectionary.cpp
@@ -16,47 +16,65 @@ using namespace std;
 string str;
 
 void process(vector<string> palabras){
-	int len=palabras[0].length;
+	/*
+	Metodo process: procesa cada grupo con los requerimientos
+			que son, el de ordenar al grupo por la ultima letra de la palabra
+
+	Atributos:
+		palabras -> vector principal de palabras
+	*/
+
+	//buscando la maxima longitud de una palabra en el subgrupo 
+	int len=palabras[0].length();
 	for(int i=1;i<palabras.size();i++){
-		len=max(len, palabras[i].length);
-		//if(palabras[i].length()>len)
-		//	len=palabras[i].length();
+		if(palabras[i].length()>len)
+			len=palabras[i].length();
 	}
 
+	//Se invierte cada palabra para hacer un sort simple
 	for(int i=0;i<palabras.size();i++)
 		reverse(palabras[i].begin(), palabras[i].end());
 
+	//se ordenan las palabras
 	sort(palabras.begin(), palabras.end());
 
+	//se vuelve a hacer un reverse, para que queden como inicialmente estaban
 	for(int i=0;i<palabras.size();i++)
 		reverse(palabras[i].begin(), palabras[i].end());
 
+	//Coloca espacios vacios en cada palabra
 	for(int i=0;i<palabras.size();i++){
-		string gg;
-		gg.resize(len - palabras[i].length(), ' ');
-		gg+=palabras[i];
-		swap(palabras[i], gg);
+		string newStr;
+		newStr.resize(len - palabras[i].length(), ' ');//los espacios que faltan se agregan
+		newStr+=palabras[i];//al final se agrega la palabra normal
+		swap(palabras[i], newStr);//se cambia por el resultado final
 	}
 
+	//imprimiendo las palabras en el orden dado
 	for(int i=0;i<palabras.size();i++)
-		cpalabras<<palabras[i]<<endl;
+		cout<<palabras[i]<<endl;
 }
 
 int main(){
+	/*
+	Metodo main: principal
+	*/
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	
-	vector<string> palabras;
+	vector<string> palabras;//vector para la salida de palabras
 
-	while(getline(cin, str)){
-		if(str.empty()){
-			process(palabras);
-			palabras.clear();
-			cpalabras<<endl;
+	while(getline(cin, str)){//siempre que haya entrada para una palabra
+		if(str.empty()){//si es salto de linea, entonces se procesa al grupo
+			process(palabras);//procesa las palabras en el vector 'palabras'
+			palabras.clear();//limpia el vector
+			cout<<endl;//salto de linea
 		}else{
-			palabras.push_back(str);
+			palabras.push_back(str);//agrega una palabra al vector 'palabras'
 		}
 	}
-	process(palabras);
+	process(palabras);//el grupo final se procesa
+
+	
 	return 0;
 }
