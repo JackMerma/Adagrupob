ea29cd473105462a04da49849313a13abadd6c42
Jackson Merma <jmermap@unsa.edu.pe>
2021-12-22 15:58:51 -0500

    documentacion y comentarios agregados en stringMatching

--- practicas/lab_11_strings/StringMatching/stringMatching.cpp
+++ practicas/lab_11_strings/StringMatching/stringMatching.cpp
@@ -8,22 +8,33 @@ using namespace std;
 #define PI 3.1415926535897932384626
 
 int main(){
+    /*
+    Metodo main: principal
+    */
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	
-	string str1,str2;
-    vector<int> matchPositions(1000000);
+	string str1,str2;//ambos strings
+    vector<int> matchPositions(1000000);//todos los posibles matches
 
-    while(getline(cin,str1), getline(cin, str2)){
+    while(getline(cin,str1), getline(cin, str2)){//siempre que se pueda leer dos lineas
+        //la pos indicara desde donde buscar en la palabra
+        //el conteo se llevara para alamcenar los matches encontrados
         int pos=0,conteo=0;
 
-        while(str2.find(str1,conteo)!=-1 && conteo<str2.size()){
+        //siempre que haya una coincidencia y 'pos' no sobrepase a str2
+        while(str2.find(str1,pos)!=-1 && pos<str2.size()){
 
-            matchPositions[pos++]=str2.find(str1,conteo);
-            conteo=matchPositions[pos-1]+1;
+            //se almacena la coincidencia
+            matchPositions[conteo++]=str2.find(str1,pos);
+            
+            //se cambia la posicion por la encontrada + 1
+            //ahora buscara desde esa posicion + 1 hasta el final  
+            pos=matchPositions[conteo-1]+1;
         }
 
-        for(int i=0;i<pos;i++)
+        //se imprimen todas las coincidencias almacenadas
+        for(int i=0;i<conteo;i++)
         	cout<<matchPositions[i]<<" ";
         
         cout<<endl;
